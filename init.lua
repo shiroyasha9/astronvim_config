dda72d0f1245ec6427ccb91eb0a3e1c45a322f53
Mubin Ansari <mubinansari910@gmail.com>
2023-08-23 01:11:33 +0530

    Add custom keybinding for C+d and Ctrl+u

--- init.lua
+++ init.lua
@@ -81,5 +81,7 @@ return {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
     -- }
+    vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
   end,
 }
