c666e91d92ba7413b5ce5ff615276968c29f38ba
Mubin Ansari <mubinansari910@gmail.com>
2023-08-29 01:19:00 +0530

    Add mappings to move lines quickly and switch between buffers

--- mappings.lua
+++ mappings.lua
@@ -9,14 +9,14 @@ return {
     -- second key is the lefthand side of the map
 
     -- navigate buffer tabs with `H` and `L`
-    -- L = {
-    --   function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
-    --   desc = "Next buffer",
-    -- },
-    -- H = {
-    --   function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
-    --   desc = "Previous buffer",
-    -- },
+    L = {
+      function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+      desc = "Next buffer",
+    },
+    H = {
+      function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+      desc = "Previous buffer",
+    },
 
     -- mappings seen under group name "Buffer"
     ["<leader>bD"] = {
@@ -38,4 +38,8 @@ return {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
   },
+  v  = {
+    ["J"] = {":m '>+1<CR>gv=gv", desc = "Move line down"},
+    ["K"] = {":m '<-2<CR>gv=gv", desc = "Move line up"},
+  }
 }
