ab322c8161bc03e8258df4a43c0e84f7fa22155d
Mubin Ansari <mubinansari910@gmail.com>
2023-09-19 18:38:15 +0530

    Add colorscheme catppuccin

--- init.lua
+++ init.lua
@@ -18,7 +18,8 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  -- colorscheme = "tokyonight-storm",
+  colorscheme = "catppuccin-macchiato",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
